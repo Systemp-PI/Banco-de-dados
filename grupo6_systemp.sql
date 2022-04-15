ebeecd5d1bcb6b736713bfb8f5b691d4c0b6fd84
Lucas Evangelista <99891098+EvangelistaL@users.noreply.github.com>
2022-04-14 21:58:16 -0300

    Script com dados inseridos

--- grupo6_systemp.sql
+++ grupo6_systemp.sql
@@ -2,8 +2,8 @@ create database systemp;
 use systemp;
 
 create table empresa (idcliente int primary key auto_increment,
-razao_social varchar(50),
-cnpj char(14),
+razao_social varchar(50) not null,
+cnpj char(14) unique,
 inscricao_estadual char(9),
 contato_nome varchar(30),
 contato2_nome varchar(30),
@@ -12,9 +12,13 @@ contato_tel varchar(15),
 estado char(2),
 cidade varchar (30));
 
+insert into empresa values (null, 'Industria Intubaia', '12123456000290', '214365870', 'Marcelo Vieira', 'Mariana Silva', 'fabrica1@intubaia.ind.br', '11921436587', 'MG', 'Extrema');
+
 select * from empresa;
 
 create table usuario (idcadastro int primary key auto_increment,
+Nome_usuario varchar(40),
+CNPJ char(14),
 email varchar(50),
 senha varchar(30),
 fk_cliente int,
@@ -28,7 +32,10 @@ local_maquina varchar(30),
 temp_min char(4),
 temp_max char(4),
 fk_empresa int,
-foreign key (fk_empresa) references empresa (idcliente));
+foreign key (fk_empresa) references empresa (idcliente))
+auto_increment = 1000;
+
+insert into sensor values (null, 'maq_03', '40°C', '55°C', '1');
 
 select * from sensor;
 desc sensor;
@@ -48,3 +55,11 @@ cpf char(14),
 cargo varchar (45),
 chefe varchar(45),
 fk_chefe int);
+
+insert into colaboradores values (null, 'Pedro Lucas Da Silva Pires', '333.333.333-33', 'Desenvolvedor Front-end', null, '6');
+
+alter table colaboradores change fk_chefe fkChefe int, add foreign key (fkChefe) references colaboradores (idcolaborador);
+
+delete from colaboradores where idColaborador = 5;
+
+select * from colaboradores; 
