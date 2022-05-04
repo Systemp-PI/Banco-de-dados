22fd697d79808d7c67fa2baed008c254d9a5cd7a
Gabriel Nogueira <gabriel.costa@sptech.school>
2022-05-04 20:56:20 -0300

    atualização modelagem e script

--- grupo6_systemp.sql
+++ grupo6_systemp.sql
@@ -6,13 +6,13 @@ razao_social varchar(50) not null,
 cnpj char(14) unique,
 inscricao_estadual char(9),
 contato_nome varchar(30),
+contato_email varchar (40),
+contato_tel varchar (15),
 contato2_nome varchar(30),
-contato_email varchar(40),
-contato_tel varchar(15),
+contato2_email varchar(40),
+contato2_tel varchar(15),
 estado char(2),
 cidade varchar (30));
-
-insert into empresas values (null, 'Industria Intubaia', '12123456000290', '214365870', 'Marcelo Vieira', 'Mariana Silva', 'fabrica1@intubaia.ind.br', '11921436587', 'MG', 'Extrema');
 
 select * from empresa;
 
@@ -27,6 +27,15 @@ foreign key (fk_cliente) references empresas (idcliente));
 select * from usuario;
 desc usuario;
 
+create table maquina(idmaquina int primary key auto_increment,
+nome_maquina varchar (25),
+temp_max char (4),
+temp_min char (4),
+fk_cliente int,
+foreign key (fk_cliente) references empresas (idcliente));
+
+select * from maquina;
+
 create table sensor (idsensor int primary key auto_increment,
 fk_maquina int,
 foreign key (fk_maquina) references maquina (idmaquina))
@@ -36,24 +45,12 @@ select * from sensor;
 desc sensor;
 
 create table log_temperatura (idregistro int primary key auto_increment,
-registro_temp char(4),
+registro_temp float,
 data_hora_registro datetime default current_timestamp,
 fk_sensor int,
 foreign key (fk_sensor) references sensor (idsensor));
 
 select * from log_temperatura;
 desc sensor;
-
-create table maquina(idmaquina int primary key auto_increment,
-nome_maquina varchar (25),
-temp_max char (4),
-temp_min char (4),
-fk_cliente int,
-foreign key (fk_cliente) references empresas (idcliente));
-
-insert into maquina 
-values (null, 'maq_1', '50°C', '41°C', '1');
-
-select * from maquina;
 
 drop database systemp;
